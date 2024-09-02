04cca7301d91a8a718fb7e24ca6ce66ac5476409
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-09-02 16:38:15 +0300

    add: db connection

---
+++ BulkyWeb/BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        { 
+
+        }
+
+        public DbSet<Categoty> Categoties { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoty>().HasData(
+                new Categoty { Id = 1, Name = "Action1", DisplayOrder = 1 },
+                new Categoty { Id = 2, Name = "Action2", DisplayOrder = 2 },
+                new Categoty { Id = 3, Name = "Action3", DisplayOrder = 3 },
+                new Categoty { Id = 4, Name = "Action4", DisplayOrder = 4 }
+                );
+            //base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ BulkyWeb/BulkyWeb/Migrations/20240902132529_SeedCategoryTable.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categoties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DisplayOrder = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categoties", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categoties",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Action1" },
+                    { 2, 2, "Action2" },
+                    { 3, 3, "Action3" },
+                    { 4, 4, "Action4" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categoties");
+        }
+    }
+}
